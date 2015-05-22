0adae059e06474c5f6d63b5122d2f7f5e445630b
José Carlos Quichiz Santome <josec.quichiz@gmail.com>
2015-05-22 03:08:49 -0500

    Implementado datepicker

--- js/principal.js
+++ js/principal.js
@@ -3,13 +3,14 @@ var pantallaActual = 0,
 	configuracion = {},
 	reconfigurar = false;
 
-//Objetos
+//Objetos DOM
 var pantallas = document.getElementsByClassName('pantalla'),
 	general = document.getElementById("general"),
 	contenedor = document.getElementById("contenedor"),
 	modal = document.getElementById("modal"),
 	contenido = document.getElementsByClassName("contenido"),
-	eventosMobile = new Hammer(general);
+	formularioDatosIniciales = document.getElementById("datosIniciales"),
+	formularioOperaciones = document.getElementById("formularioOperaciones");
 
 //Funciones
 var dimensionarDivContenido = function(callback){
@@ -29,6 +30,8 @@ var dimensionarDivContenido = function(callback){
 		return cant;
 	},
 	inicializar = function(reconf){
+		formularioDatosIniciales.reset();
+		formularioOperaciones.reset();
 		reconf = reconf || false;
 		this.reconfigurar = reconf;
 		Velocity(modal, "fadeIn", {duration: 10});
@@ -63,11 +66,33 @@ var dimensionarDivContenido = function(callback){
 			var regex = /^[0-9]+([\.][0-9]+)?$/m;
 			return regex.test(num);
 		}
+	},
+	aDosDigitos = function(numero){
+		var str = "" + numero;
+		var pad = "00";
+		return pad.substring(0, pad.length - str.length) + str;
 	};
 
+//Objetos de Plugin
+var eventosMobile = new Hammer(general),
+	selFecha = new Pikaday(
+		{
+			field: document.getElementById('fecha'),
+			format: 'DD/MM/YY',
+			i18n: {
+			    previousMonth : 'Mes anterior',
+			    nextMonth     : 'Mes siguiente',
+			    months        : ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Setiembre','Octubre','Noviembre','Diciembre'],
+			    weekdays      : ['Lunes','Martes','Miercoles','Jueves','Viernes','Sábado','Domingo'],
+			    weekdaysShort : ['Lun','Mar','Mie','Jue','Vie','Sab','Dom']
+			},
+			onSelect: function(date) {
+		        document.getElementById('fecha').value = aDosDigitos(date.getDate())+"/"+aDosDigitos(date.getMonth()+1)+"/"+date.getFullYear();
+		    }
+			})
 
 //Listeners
-document.getElementById("datosIniciales").onsubmit = function(e){
+formularioDatosIniciales.onsubmit = function(e){
 	e.preventDefault();
 	var dataSrc = e.target,
 		empresa = dataSrc[0].value,
